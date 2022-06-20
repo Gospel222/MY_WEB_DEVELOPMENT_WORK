5b2d0dd6740ce05f907e84fd0da288cb2ec6461d
Gospel <107857863+Gospel222@users.noreply.github.com>
2022-06-20 13:32:26 +0100

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,37 @@
+print('hello world')
+
+print('My Name is Victor Wilkis')
+
+
+#int => intergers are Whole Numbers:
+print(25,30,35,40,45)
+
+#float => Floats are Decimal Numbers:
+print(10.0,20.5,30.5,40.5)
+
+#Strings => using Single Quotation Marks:
+print('Victor Wilkis')
+
+#Strings => using Double Quotation Marks:
+print("Miss Jennifer")
+
+#Variables:
+name = 'Joy'
+print(name)
+
+#Statement: Escaping Special Characters with backward Slash
+print('Jennifer\'s Bag')
+
+#Escaping Special Characters with double quotation marks:
+print("Jennifer's Book")
+
+print("Rome was not built in a day")
+
+#Assignment:
+
+# 1. write your name as a string using single quotation marks.
+# 2. write your surname as a string using double quotation marks.
+# 3. write your phone number as an int 
+# 4. create a variable called state with your state of origin as the value.
+
+input()
